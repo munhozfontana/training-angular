3beb564ebce538252a2f42bee39b0e912f07cece
munhozfontana <munhozfontana@gmail.com>
2020-12-02 13:19:28 -0300

    5. add In-app Navigation

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,7 +11,11 @@ export class HeroService {
   constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find((hero) => hero.id === id) || ({} as Hero));
   }
 }
