cf60b5f85dc4b664db872b579eefdd35e6ef15cc
munhozfontana <munhozfontana@gmail.com>
2020-12-02 16:49:07 -0300

    6. Get Data from a Server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -23,13 +23,15 @@ export class HeroDetailComponent implements OnInit {
   }
 
   getHero(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.heroService
-      .getHero(Number(id))
-      .subscribe((hero) => (this.hero = hero));
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
   }
 }
